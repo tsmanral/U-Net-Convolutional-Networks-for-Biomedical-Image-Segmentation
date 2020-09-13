ed6c20d103a643f779fb975c578e1f6efe601416
Tribhuwan Singh <tribhuwan50@gmail.com>
2020-09-12 19:12:20 -0700

    Final commit

--- UNet.py
+++ UNet.py
@@ -14,6 +14,15 @@ def double_conv(in_c, out_c):
     return conv
 
 
+def crop_img(tensor, target_tensor):
+    target_size = target_tensor.size()[2]
+    tensor_size = tensor.size()[2]
+    delta = tensor_size - target_size
+    delta = delta // 2
+    return tensor[:, :, delta:tensor_size-delta, delta:tensor_size-delta]
+    
+
+
 class UNet(nn.Module):
     def __init__(self):
         super(UNet, self).__init__()
@@ -25,9 +34,48 @@ class UNet(nn.Module):
         self.down_conv_4 = double_conv(256, 512)
         self.down_conv_5 = double_conv(512, 1024)
         
-        self.up_trans_1 = nn.ConvTranspose2d(in_channels=) 
+        self.up_trans_1 = nn.ConvTranspose2d(
+            in_channels = 1024, 
+            out_channels = 512, 
+            kernel_size = 2,
+            stride = 2) 
+
+        self.up_conv_1 = double_conv(1024, 512)
+
+        self.up_trans_2 = nn.ConvTranspose2d(
+            in_channels = 512, 
+            out_channels = 256, 
+            kernel_size = 2,
+            stride = 2) 
+
+        self.up_conv_2 = double_conv(512, 256)
+
+        self.up_trans_3 = nn.ConvTranspose2d(
+            in_channels = 256, 
+            out_channels = 128, 
+            kernel_size = 2,
+            stride = 2) 
+
+        self.up_conv_3 = double_conv(256, 128)
+
+        self.up_trans_4 = nn.ConvTranspose2d(
+            in_channels = 128, 
+            out_channels = 64, 
+            kernel_size = 2,
+            stride = 2) 
+
+        self.up_conv_4 = double_conv(128, 64)
+
+        self.out = nn.Conv2d(
+            in_channels = 64,
+            out_channels = 2,
+            kernel_size = 1
+        )
+
+
 
     def forward(self, image):
+        # batch_size, channel, height, width
         # Encoder 
         x1 = self.down_conv_1(image)  #
         x2 = self.max_pool_2x2(x1)
@@ -38,6 +86,28 @@ class UNet(nn.Module):
         x7 = self.down_conv_4(x6)  #
         x8 = self.max_pool_2x2(x7)
         x9 = self.down_conv_5(x8)  
+        
+        # Decoder
+        x = self.up_trans_1(x9)
+        y = crop_img(x7, x)
+        x = self.up_conv_1(torch.cat([x, y], 1))
+
+        x = self.up_trans_2(x)
+        y = crop_img(x5, x)
+        x = self.up_conv_2(torch.cat([x, y], 1))
+
+        x = self.up_trans_3(x)
+        y = crop_img(x3, x)
+        x = self.up_conv_3(torch.cat([x, y], 1))
+
+        x = self.up_trans_4(x)
+        y = crop_img(x1, x)
+        x = self.up_conv_4(torch.cat([x, y], 1))
+
+        x = self.out(x)
+        print(x.size())
+        return x
+
 
 
 
